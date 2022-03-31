efcafd9ab6939e90960a8ffe98f8fae6c6c4b20b
milkyboy94 <53465675+milkyboy94@users.noreply.github.com>
2022-03-31 18:50:22 +0900

    image-r1

--- js/background.js
+++ js/background.js
@@ -1,8 +1,9 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+bgImage.classList.add("bgImage");
 
 bgImage.src = `img/${chosenImage}`;
 
